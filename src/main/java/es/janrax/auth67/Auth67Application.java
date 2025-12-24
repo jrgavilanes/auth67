9ba4dbe339526d6e14c537ea34b05a89d0703ccb
jrgavilanes@gmail.com <jrgavilanes@gmail.com>
2025-12-24 10:06:46 +0100

    base project

---
+++ src/main/java/es/janrax/auth67/Auth67Application.java
@@ -0,0 +1,13 @@
+package es.janrax.auth67;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth67Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth67Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/janrax/auth67/Auth67ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.janrax.auth67;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth67ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
